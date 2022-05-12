e91ffc33dc6a2a73092787ab58c3a2915d493ca3
gingercook <vahitaras@gmail.com>
2022-05-12 14:53:18 -0400

    custom hook is created

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -1,11 +1,22 @@
 import { useState, useEffect } from 'react';
 
+// this stores all global values
 let globalState = {};
 let listeners = [];
+// This stores functions
 let actions = {};
 
-const useStore = () => {
+export const useStore = () => {
     const setState = useState(globalState)[1];
+
+    const dispatch = (actionIdentifier, payload) => {
+        const newState = actions[actionIdentifier](globalState, payload);
+        globalState = {...globalState, ...newState};
+
+        for (const listener of listeners) {
+           listener(globalState);
+        };
+    };
 
     useEffect(() => {
         listeners.push(setState);
@@ -13,5 +24,15 @@ const useStore = () => {
         return () => {
             listeners = listeners.filter(li => li !== setState);
         };
-    }, [])
+    }, []);
+
+    return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+    if (initialState) {
+        globalState = {...globalState, initialState};
+    }
+
+    actions = {...actions, ...userActions};
 };
